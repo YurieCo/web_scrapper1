e80a9dec75d440c8e66d31031e8b44a084f34a68
Iurii <iurii@27.0.168.192.in-addr.arpa>
2018-06-14 09:38:13 +0300

    test

--- scraper.py
+++ scraper.py
@@ -22,3 +22,22 @@
 # All that matters is that your final data is written to an SQLite database
 # called "data.sqlite" in the current working directory which has at least a table
 # called "data".
+from requests_html import HTMLSession
+
+base_url = ' https://www.zomato.com/melbourne/dinner-in-ashburton'
+
+headers = {
+    "Host": "www.zomato.com",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:60.0) Gecko/20100101 Firefox/60.0",
+"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+"Accept-Language": "en-GB,en;q=0.5",
+"Accept-Encoding": "gzip, deflate, br",
+"Connection": "keep-alive",
+"Upgrade-Insecure-Requests": "1"
+}
+
+session = HTMLSession()
+r = session.get(base_url, headers = headers)
+r.html.render()
+print(r.ok, r.status_code)
+
